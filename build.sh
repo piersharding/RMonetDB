26977407219a33e06abd846874f54577a4766408
Piers Harding <piers@catalyst.net.nz>
2013-02-01 18:15:38 +1300

    fix up build issues caused by autoconf

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+#export MAPI_INCLUDE=/home/piers/code/sap/nwrfcsdk/include
+#export MAPI_LIBS=/home/piers/code/sap/nwrfcsdk/lib
+
 rm -rf /home/piers/R/x86_64-pc-linux-gnu-library/2.15/RMonetDB
 rm src/RMonetDB.o
 rm src/RMonetDB.so
@@ -9,4 +12,5 @@ rm configure
 autoconf
 
 R CMD INSTALL --build --preclean --clean .
+#R CMD INSTALL --build --preclean --clean  --configure-args='--with-mapi-include=/home/piers/code/sap/nwrfcsdk/include --with-mapi-lib=/home/piers/code/sap/nwrfcsdk/lib' .
 
